0d602bed9fab134016827857785e9121d4040a87
Jeremy <jeremyasuncion808@gmail.com>
2024-10-19 01:49:46 -0700

    sync files script

--- sync-files.fish
+++ sync-files.fish
@@ -1,3 +1,23 @@
 #!/usr/bin/env fish
 
-nodemon -e '*' -x 'fish -c "copy . lilnas.io:./lilnas"'
+function sync_files
+  set repo_dir (gr)
+  set watch false
+
+  for arg in $argv
+    switch $arg
+      case '-w' '--watch'
+        set watch true
+        break
+    end
+  end
+
+  if $watch
+    nodemon -e '*' -x "fish -c 'copy $repo_dir/ lilnas.io:./lilnas/'"
+    return
+  end
+
+  copy $repo_dir/ lilnas.io:./lilnas/
+end
+
+sync_files $argv
