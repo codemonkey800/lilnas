4cffa33931d93b47083eceaa38f51d85cbf0dc3c
Jeremy <jeremyasuncion808@gmail.com>
2025-02-17 02:09:20 -0800

    fix date format

--- tdr-bot/src/message-handler/tools.ts
+++ tdr-bot/src/message-handler/tools.ts
@@ -1,6 +1,8 @@
 import { tool } from '@langchain/core/tools'
+import dayjs from 'dayjs'
 
-export const dateTool = tool(() => new Date().toISOString(), {
+export const dateTool = tool(() => dayjs().format('MMMM DD, YYYY hh:mm:ss A'), {
   name: 'get_date',
-  description: 'Gets the current date in ISO format',
+  description:
+    'Gets the current PST date in the format <Month> <Day>, <Year> <Hour>:<Minute>:<Second> [AM/PM]',
 })
