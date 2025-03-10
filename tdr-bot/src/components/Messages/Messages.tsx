a23bc09fd44ee19b9420fd47d0728c0ecb11fed0
Jeremy <jeremyasuncion808@gmail.com>
2025-03-10 01:06:15 -0700

    add no messages label

--- tdr-bot/src/components/Messages/Messages.tsx
+++ tdr-bot/src/components/Messages/Messages.tsx
@@ -9,6 +9,14 @@ export async function Messages() {
 
   return (
     <div className="flex flex-col gap-4">
+      {messages.length === 0 && (
+        <div className="flex items-center justify-center h-[30vh]">
+          <p className="text-4xl font-medium w-full text-center">
+            no messages 😭
+          </p>
+        </div>
+      )}
+
       {messages.map((message) => (
         <MessageCard
           key={message.id ?? JSON.stringify(message.content)}
