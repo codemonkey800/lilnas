8da91cf896b6d93ee585d81039a25a065345b261
Jeremy <jeremyasuncion808@gmail.com>
2025-03-12 13:09:49 -0700

    add -w flag to sync-dev-files

--- .env.fish
+++ .env.fish
@@ -14,17 +14,30 @@ end
 function sync-dev-files
   set root_dir (get_root_dir)
   set app_name (get_app_name)
+  set watch false
+
+  for arg in $argv
+    switch $arg
+      case '-w' '--watch'
+        set watch true
+    end
+  end
 
   if test "$app_name" = ''
     echo "Could not determine app name"
     return 1
   end
 
-  set ignore_files 'js,ts,jsx,tsx,json,yml'
+  set watch_files 'js,ts,jsx,tsx,json,yml'
   set copy_args "--delete --exclude .git --exclude node_modules $root_dir/$app_name/ lilnas.io:./dev/$app_name/"
+
+  if not $watch
+    fish -c "copy $copy_args"
+    return
+  end
 
   nodemon \
     --watch $root_dir \
-    --ext $ignore_files \
+    --ext $watch_files \
     --exec "fish -c 'copy $copy_args'"
 end
