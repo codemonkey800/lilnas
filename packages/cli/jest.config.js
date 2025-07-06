5843ea73e3150b6f3506691b37f9daa919eb1705
Jeremy <jeremyasuncion808@gmail.com>
2025-07-06 16:30:08 -0700

    prettify jest config

--- packages/cli/jest.config.js
+++ packages/cli/jest.config.js
@@ -2,7 +2,11 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.ts', '**/?(*.)+(spec|test).ts', '!**/__tests__/setup.ts'],
+  testMatch: [
+    '**/__tests__/**/*.ts',
+    '**/?(*.)+(spec|test).ts',
+    '!**/__tests__/setup.ts',
+  ],
   transform: {
     '^.+\\.ts$': 'ts-jest',
   },
