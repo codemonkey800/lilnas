35729d12d9ca6e6a7aed181e638bb9beb460c22b
Jeremy <jeremyasuncion808@gmail.com>
2026-03-14 14:25:52 -0700

    fix yoink migration

--- apps/yoink/drizzle/0001_silly_hairball.sql
+++ apps/yoink/drizzle/0001_silly_hairball.sql
@@ -1,3 +1,7 @@
+UPDATE "download_search_result" SET "tmdb_id" = 0 WHERE "tmdb_id" IS NULL;--> statement-breakpoint
+UPDATE "download_search_result" SET "tvdb_id" = 0 WHERE "tvdb_id" IS NULL;--> statement-breakpoint
+UPDATE "download_search_result" SET "season_number" = 0 WHERE "season_number" IS NULL;--> statement-breakpoint
+UPDATE "download_search_result" SET "episode_number" = 0 WHERE "episode_number" IS NULL;--> statement-breakpoint
 ALTER TABLE "download_search_result" ALTER COLUMN "tmdb_id" SET DEFAULT 0;--> statement-breakpoint
 ALTER TABLE "download_search_result" ALTER COLUMN "tmdb_id" SET NOT NULL;--> statement-breakpoint
 ALTER TABLE "download_search_result" ALTER COLUMN "tvdb_id" SET DEFAULT 0;--> statement-breakpoint
