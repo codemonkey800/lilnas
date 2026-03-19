1299b8aabf92356a7e34b3e8a09e0c1badbd64c9
Jeremy <jeremyasuncion808@gmail.com>
2026-03-19 01:00:14 -0700

    help flag -h

--- packages/cli/bin/run.js
+++ packages/cli/bin/run.js
@@ -7,9 +7,10 @@ const { execute } = require('@oclif/core')
 
 const args = process.argv.slice(2)
 
-if (args[0] === 'help') {
+if (args[0] === 'help' || args[0] === '-h') {
   // `lilnas help` → `lilnas --help`
   // `lilnas help redeploy` → `lilnas redeploy --help`
+  // `lilnas -h` → `lilnas --help`
   process.argv.splice(2, 1)
   process.argv.push('--help')
 }
